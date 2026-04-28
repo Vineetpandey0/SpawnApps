21683dedbeacbeb2e1c0355dd4ad830c34f0ff56
Vineet Pandey <vineetpandey0090@gmail.com>
2026-04-28 23:00:43 +0530

    feat: added lenis for smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalNavbar from "@/components/builder/GlobalNavbar";
 import Footer from "@/components/builder/Footer";
+import LenisScroll from "@/components/lenis";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <GlobalNavbar />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <LenisScroll />
+        {children}
+      </body>
       <Footer />
     </html>
   );
