599c81dad1c41645dbabeda0e95554055e7f3740
Vineet Pandey <vineetpandey0090@gmail.com>
2026-04-30 13:59:43 +0530

    fix: authentication fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,19 @@
 import BuildProcess from '@/components/builder/sections/Build-process'
 import HeroSection from '@/components/builder/sections/HeroSection'
 import GlobalNavbar from '@/components/builder/GlobalNavbar'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '@/components/builder/Footer'
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 
 function page() {
+  const {user} = useUser();
+  const router = useRouter();
+  useEffect(()=>{
+    if(user){
+      router.push('/dashboard')
+    }
+  },[user])
   return (
     <div>
       <GlobalNavbar />
