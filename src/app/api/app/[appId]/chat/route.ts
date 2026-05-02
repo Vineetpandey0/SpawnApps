332f42522551c4168ff9d1f0d0e528f9187d0308
Vineet Pandey <vineetpandey0090@gmail.com>
2026-05-02 13:32:17 +0530

    fix: chat load bugs fixed

--- src/app/api/app/[appId]/chat/route.ts
+++ src/app/api/app/[appId]/chat/route.ts
@@ -1,13 +1,17 @@
 import { prisma } from "@/lib/db/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import React from "react";
 
-export async function GET(req: NextRequest) {
-    const {userId} = await auth()
+export async function GET(req: NextRequest,
+    context: { params: Promise<{ appId: string }> }
+) {
+    const {appId} = await context.params;
+    console.log("Fetching chats for appId:", appId);
     try {
         const res = await prisma.chats.findMany({
             where: {
-                userId: userId
+                appId: appId
             }
         })
         return NextResponse.json({ chats: res }, { status: 200 })
