46f247d58884cceee815d940075bff36e95cad49
Vineet Pandey <vineetpandey0090@gmail.com>
2026-05-04 10:19:47 +0530

    added redirects to primary url www.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'spawnapps.dev' }],
+        destination: 'https://www.spawnapps.dev/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
